86f27b817f3262939cda945a7939f33590e1145b
Vishnu868 <135257497+Vishnu868@users.noreply.github.com>
2025-04-10 10:48:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 echo "📥 Downloading model from Google Drive..."
 python download_model.py
 
+echo "📥 Cloning YOLOv5 repository..."
+git clone https://github.com/ultralytics/yolov5
+
 echo "🚀 Starting Flask server..."
 python app.py
