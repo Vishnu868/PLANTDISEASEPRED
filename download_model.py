55b09edca398d20b0d51537b73d872a3b1c83544
Vishnu868 <135257497+Vishnu868@users.noreply.github.com>
2025-04-11 20:48:55 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,6 +1,6 @@
 # download_model.py
 import gdown
 
-url = 'url = 'https://drive.google.com/uc?id=1eWnEpZ3Yxny9MaQgtjYsBTKJxWczPP9J'
+url = 'https://drive.google.com/uc?id=1eWnEpZ3Yxny9MaQgtjYsBTKJxWczPP9J'
 output = 'best.pt'
 gdown.download(url, output, quiet=False)
